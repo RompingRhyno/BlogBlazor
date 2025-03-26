03189e7ab5c1729d9b4832b2cde03b3ee5014d90
Thai Hoa Nguyen <tnguyen568@my.bcit.ca>
2025-03-26 02:28:31 -0700

    Some fixes

--- BlogServer/Services/UserService.cs
+++ BlogServer/Services/UserService.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Ganss.Xss;
 using BlogServer.Data;
 using BlogLibrary.Models;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlogServer.Services
 {
@@ -10,11 +11,17 @@ namespace BlogServer.Services
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<User> _userManager;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
 
-        public UserService(ApplicationDbContext context, UserManager<User> userManager)
+
+        public UserService(
+            ApplicationDbContext context, 
+            UserManager<User> userManager,
+            AuthenticationStateProvider authenticationStateProvider)
         {
             _context = context;
             _userManager = userManager;
+            _authenticationStateProvider = authenticationStateProvider;
         }
     
         public async Task<List<Article>> GetAllArticlesAsync()
@@ -106,176 +113,12 @@ namespace BlogServer.Services
             await _context.SaveChangesAsync();
             return true;
         }
+
+        public async Task<string> GetCurrentUsername()
+        {
+            // Get from authentication state
+            var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+            return authState.User.Identity?.Name ?? string.Empty;
+        }
     }
 }
-
-    // [HttpPost]
-    // [Authorize(Roles = "Contributor,Admin")]
-    // [ValidateAntiForgeryToken]
-    // public async Task<IActionResult> Create(Article article)
-    // {   
-    //     if (User.Identity == null)
-    //     {
-    //         return Forbid();
-    //     }
-    //     article.ContributorUsername = User.Identity.Name;
-    //     ModelState.Remove("ContributorUsername");  // Remove from validation if necessary
-        
-    //     if (ModelState.IsValid)
-    //     {
-    //         if (article.StartDate.HasValue && article.EndDate.HasValue && article.EndDate < article.StartDate)
-    //         {
-    //             ModelState.AddModelError("EndDate", "End Date must be after Start Date.");
-    //             return View(article);
-    //         }
-
-    //         // Sanitize the article Body content to prevent XSS
-    //         var sanitizer = new HtmlSanitizer();
-    //         article.Body = sanitizer.Sanitize(article.Body ?? string.Empty);  // Fallback to empty string if Body is null
-    //         var user = await _userManager.GetUserAsync(User);
-
-    //         if (user == null)
-    //         {
-    //             return Forbid();
-    //         }
-
-    //         article.Contributor = user;
-    //         article.ContributorUsername = user.UserName!;
-    //         article.CreateDate = DateTime.UtcNow;
-
-    //         _context.Articles.Add(article);
-    //         await _context.SaveChangesAsync();
-
-    //         return RedirectToAction(nameof(Index));
-    //     }
-    //     else
-    //     {
-    //         // Print out the model errors to see what might be invalid
-    //         foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
-    //         {
-    //             Console.WriteLine($"Error: {error.ErrorMessage}");
-    //         }
-    //     }
-
-    //     return View(article);
-    // }
-
-//     [HttpGet]
-//     [Authorize(Roles = "Contributor,Admin")]
-//     public async Task<IActionResult> Edit(int id)
-//     {
-//         var article = await _context.Articles.FindAsync(id);
-
-//         if (article == null)
-//         {
-//             return NotFound();
-//         }
-
-//         // Check if the user identity is null
-//         if (User.Identity?.Name == null)
-//         {
-//             return Forbid();
-//         }
-
-//         // Ensure the user is either the contributor or an admin
-//         if (article.ContributorUsername != User.Identity.Name && !User.IsInRole("Admin"))
-//         {
-//             var user = await _userManager.GetUserAsync(User);
-//             var username = user?.UserName;
-//             var contributorUsername = article.ContributorUsername;
-//             Console.WriteLine(username);
-//             Console.WriteLine(contributorUsername);
-//             Console.WriteLine("User is not the contributor or an admin");
-//             return Forbid();
-//         }
-
-//         return View(article);
-//     }
-
-//     [HttpPost]
-//     [Authorize(Roles = "Contributor,Admin")]
-//     [ValidateAntiForgeryToken]
-//     public async Task<IActionResult> Edit(int id, Article article)
-//     {
-//         if (id != article.ArticleId)
-//         {
-//             return NotFound();
-//         }
-
-//         var existingArticle = await _context.Articles
-//             .Include(a => a.Contributor)  // Eagerly load
-//             .FirstOrDefaultAsync(a => a.ArticleId == id);
-
-//         // Check if the article exists
-//         if (existingArticle == null)
-//         {
-//             return NotFound();
-//         }
-
-//         // Check if the user identity is null
-//         if (User.Identity?.Name == null)
-//         {
-//             return Forbid();
-//         }
-
-//         // Ensure the user is either the contributor or an admin
-//         if (existingArticle.Contributor?.UserName != User.Identity.Name && !User.IsInRole("Admin"))
-//         {
-//             return Forbid();
-//         }
-
-//         // Automatically set ContributorUsername to the logged-in user's name
-//         article.ContributorUsername = User.Identity.Name;  // Set on the passed article object
-//         Console.WriteLine(article.ContributorUsername);
-//         ModelState.Remove("ContributorUsername");  // Remove the ContributorUsername from the ModelState
-
-//         if (ModelState.IsValid)
-//         {
-//             // Validation for dates
-//             if (article.StartDate.HasValue && article.EndDate.HasValue && article.EndDate < article.StartDate)
-//             {
-//                 ModelState.AddModelError("EndDate", "End Date must be after Start Date.");
-//                 return View(article);
-//             }
-
-//             // Sanitize the article Body content to prevent XSS
-//             var sanitizer = new HtmlSanitizer();
-//             article.Body = sanitizer.Sanitize(article.Body ?? string.Empty);  // Fallback to empty string if Body is null
-
-//             // Update article properties
-//             existingArticle.Title = article.Title;
-//             existingArticle.Body = article.Body;
-//             existingArticle.StartDate = article.StartDate;
-//             existingArticle.EndDate = article.EndDate;
-
-//             // Save changes to database
-//             _context.Update(existingArticle);
-//             await _context.SaveChangesAsync();
-
-//             return RedirectToAction(nameof(Index));
-//         }
-//         return View(article);
-//     }
-
-//     [HttpPost]
-//     [Authorize(Roles = "Contributor,Admin")]
-//     [ValidateAntiForgeryToken]
-//     public async Task<IActionResult> Delete(int id)
-//     {
-//         var article = await _context.Articles.FindAsync(id);
-//         if (article == null)
-//         {
-//             return NotFound();
-//         }
-
-//         _context.Articles.Remove(article);
-//         await _context.SaveChangesAsync();
-
-//         return RedirectToAction(nameof(Index));
-//     }
-
-//     public IActionResult Privacy()
-//     {
-//         return View();
-//     }
-// }
